e19e26c38740689331ce6d2bc78c1baa909bd4fa
aislander <aislander0@outlook.com>
2022-03-18 18:04:35 +0800

    update sql connect

--- model/tokenModel.go
+++ model/tokenModel.go
@@ -9,21 +9,21 @@ type Token struct {
 
 func Token2UserId(token string) (int, error) {
 	var res Token
-	db := newDB()
+	// db := newDB()
 	err := db.Where("token = ?", token).Take(&res).Error
 	return res.UserId, err
 }
 
 func GetTokenByUserId(userId int) string {
 	var res Token
-	db := newDB()
+	// db := newDB()
 	db.Where("user_id = ?", userId).Take(&res)
 	return res.Token
 }
 
 func SaveToken(userId int, token string, time int) int {
 	res := Token{Token: token, UserId: userId, time: time}
-	db := newDB()
+	// db := newDB()
 	db.Create(&res)
 	UpdateUserToken(userId, token)
 	return res.Id
