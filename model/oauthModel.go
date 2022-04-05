41653d657176fb165739b0b37318caace3cd8ca7
aislander <aislander0@outlook.com>
2022-04-05 14:20:15 +0800

    update model db query

--- model/oauthModel.go
+++ model/oauthModel.go
@@ -9,21 +9,18 @@ type Oauth struct {
 
 func GetOauthByPassword(password string) int {
 	var res Oauth
-	db := newDB()
 	db.Where("password = ?", password).Take(&res)
 	return res.Id
 }
 
 func GetOauthByIp(ipAddr string) (int, error) {
 	var res Oauth
-	db := newDB()
 	err := db.Where("ip_address = ?", ipAddr).Take(&res).Error
 	return res.Id, err
 }
 
 func SaveOauth(userId int, password string, email string, ipAddr string) int {
 	res := Oauth{Id: userId, Password: password, Email: email, IpAddress: ipAddr}
-	db := newDB()
 	db.Create(&res)
 	return res.Id
 }
--- model/userModel.go
+++ model/userModel.go
@@ -16,33 +16,28 @@ func UpdateUser(data User) int {
 
 func GetUserArr(userIdArr []int) []User {
 	var res []User
-	db := newDB()
 	db.Find(&res, userIdArr)
 	return res
 }
 
 func (user *User) Create() int {
-	db := newDB()
 	db.Create(user)
 	return user.Id
 }
 
 func (user *User) Update() int {
-	db := newDB()
 	db.Model(user).Update("name", user.Name)
 	return user.Id
 }
 
 func GetUser(id int) User {
 	var res User
-	db := newDB()
 	db.Where("id = ?", id).Take(&res)
 
 	return res
 }
 
 func UpdateUserToken(userId int, token string) {
-	db := newDB()
 	db.Model(&User{Id: userId}).Update("token", token)
 }
 
